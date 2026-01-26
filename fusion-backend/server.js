046d14e9c861daf3c1f6d04ac1f393026cdd4e0c
sanjana rathee <sanjanarathee7@gmail.com>
2026-01-27 02:18:44 +0530

    Fix CORS for deployment

--- fusion-backend/server.js
+++ fusion-backend/server.js
@@ -25,11 +25,12 @@ const app = express();
 // ------------------------------
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 
