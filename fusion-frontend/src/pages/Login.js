ac27178303d75057d0168712b63323ce4004292f
sanjana rathee <sanjanarathee7@gmail.com>
2026-04-13 12:32:38 +0530

    final login fix

--- fusion-frontend/src/pages/Login.js
+++ fusion-frontend/src/pages/Login.js
@@ -13,11 +13,11 @@ export default function Login() {
     setMsg("");
 
     try {
-      const res = await axios.post("https://fusion-backend.onrender.com/api/auth/login", {
+      const res = await axios.post("https://fusion0-1.onrender.com/api/auth/login", {
         identifier: identifier.trim(),
         password,
       });
-
+     // ✅ NEW (correct)
       localStorage.setItem(
   "fusionUser",
   JSON.stringify({
